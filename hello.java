8105bd51f228686d0f43391fad225b51613585e4
orange5444 <nickmariasi@yahoo.com>
2017-10-23 13:44:49 -0400

    Large Program Change
    Changed the Hello World program into a name program

--- hello.java
+++ hello.java
@@ -1,12 +1,15 @@
 // a simple program to practice common Github commands 
+import java.util.Scanner;
 
 public class hello {
 
     public static void main(String [] args) {
+        String name;
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Your Name: ");
+        name = scanner1.nextLine();
 
-        System.out.println("Hello Class!");
-
-        System.out.println("Bye"); //test edit
+        System.out.println("Hello " + name + " my name is Nick");
 
     }
 }
